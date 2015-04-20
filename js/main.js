c81c2e6aad37a352a28c61f0ea25e5174b4a19d0
Dimitri Nooch La-Or <dimitrinoochlaor@gmail.com>
2015-04-20 13:33:46 -0700

    did this for fun after i finished

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 // Equivalent event setup using the `.on()` method
 $( ".dan" ).click(function() {
-alert( "Hey you clicked on me" );
+	alert( "Hey you clicked on me" );
+	$( ".dan" ).fadeOut( "slow", function() {
+	// Animation complete.
+	});
 });
 $( "#iwao" ).click(function() {
 alert( "Hey you clicked on me again i told u not to" );
+	$( "#iwao" ).fadeOut( "slow", function() {
+	// Animation complete.
+	});
 });
