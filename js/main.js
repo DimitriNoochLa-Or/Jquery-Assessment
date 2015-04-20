5be9c9bd5f936a6c185b261403501e13de0c989f
Dimitri Nooch La-Or <dimitrinoochlaor@gmail.com>
2015-04-20 13:23:34 -0700

    Finished test . if u click click?? alert pops up one below if u click alert pops up

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Equivalent event setup using the `.on()` method
+$( ".dan" ).click(function() {
+alert( "Hey you clicked on me" );
+});
+$( "#iwao" ).click(function() {
+alert( "Hey you clicked on me again i told u not to" );
+});
